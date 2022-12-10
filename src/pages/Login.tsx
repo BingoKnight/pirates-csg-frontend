b678e40071dd4ceea4833e3f1d3361d8a076c799
Nathan Hessling <nrhessling@gmail.com>
2022-12-09 21:13:06 -0500

    make login page more flexible

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,14 +10,16 @@ import '../styles/login.scss'
 function Login() {
     return (
         <Layout>
-            <div className="login-container">
-                <div className="row"><TextInput id="username-input" label="Username" className="username-input" /></div>
-                <div className="row"><PasswordInput id="password-input" label="Password" className="password-input" /></div>
-                <div className="row"><Button id="login-button">Login</Button></div>
-                <div className="row"><LinkButton id="register-button" to="/register">Register</LinkButton></div>
-                <div className="row forgot-links-row">
-                    <LinkButton className="forgot-link">Forgot Username?</LinkButton>
-                    <LinkButton className="forgot-link">Forgot Password?</LinkButton>
+            <div className="row login-container">
+                <div className="col">
+                    <div className="row"><TextInput id="username-input" label="Username" className="username-login-input" /></div>
+                    <div className="row"><PasswordInput id="password-input" label="Password" className="password-login-input" /></div>
+                    <div className="row"><Button id="login-button">Login</Button></div>
+                    <div className="row"><LinkButton id="register-button" to="/register">Register</LinkButton></div>
+                    <div className="row forgot-links-row">
+                        <LinkButton className="forgot-link">Forgot Username?</LinkButton>
+                        <LinkButton className="forgot-link">Forgot Password?</LinkButton>
+                    </div>
                 </div>
             </div>
         </Layout>
